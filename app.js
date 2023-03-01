75f16ee11a63cc0ed157b14ea3eeef3e9abeab27
starlit-nox <kdkelly.1998@gmail.com>
2023-02-28 23:09:47 -0600

    JS, HTML, CSS Added
    HTML Added
    -Basic Homepage Layout
    - Sales.HTML for JS
    
    CSS Added
    - Background Color
    - Logo
    - Nav Bar
    - Basic Layout
    
    JS Added
    - Object Literal for Data added
    - app.js added

---
+++ app.js
@@ -0,0 +1,90 @@
+                // Seattle Object Literal Begins
+                var seattleSales = {
+                    minSales: 23, 
+                    maxSales: 65,
+                    avgSales: 6.3,
+                    purchases: function(){
+                        return this.minSales + this.maxSales;
+                    },
+                    totalSales : function(){
+                        return this.minSales + this.maxSales;           
+                    }
+                };
+
+                document.write("<h2>" + " The min sales of this hour is " + seattleSales.minSales)
+                document.write("<h2>" + " The max sales of this hour is " + seattleSales.maxSales)
+                document.write("<h2>" + " The average sales of this hour is " + seattleSales.avgSales)  
+                // Seattle Object Literal Ends
+
+                // Tokyo Object Literal Begins
+                var tokyoSales = {
+                    minSales: 2, 
+                    maxSales: 24,
+                    avgSales: 1.2,
+                    purchases: function(){
+                        return this.minSales + this.maxSales;
+                    },
+                    totalSales : function(){
+                        return this.minSales + this.maxSales;           
+                    }
+                };
+
+                document.write("<h2>" + " The min sales of this hour is " + tokyoSales.minSales)
+                document.write("<h2>" + " The max sales of this hour is " + tokyoSales.maxSales)
+                document.write("<h2>" + " The average sales of this hour is " + tokyoSales.avgSales)
+                // Tokyo Object Literal Ends
+
+                // Dubai Object Literal Begins
+                var dubaiSales = {
+                    minSales: 11, 
+                    maxSales: 38,
+                    avgSales: 3.7,
+                    purchases: function(){
+                        return this.minSales + this.maxSales;
+                    },
+                    totalSales : function(){
+                        return this.minSales + this.maxSales;           
+                    }
+                };
+
+                document.write("<h2>" + " The min sales of this hour is " + dubaiSales.minSales)
+                document.write("<h2>" + " The max sales of this hour is " + dubaiSales.maxSales)
+                document.write("<h2>" + " The average sales of this hour is " + dubaiSales.avgSales)
+                // Dubai Object Literal Ends
+
+                // Paris Object Literal Begins
+                var parisSales = {
+                    minSales: 20, 
+                    maxSales: 38,
+                    avgSales: 2.3,
+                    purchases: function(){
+                        return this.minSales + this.maxSales;
+                    },
+                    totalSales : function(){
+                        return this.minSales + this.maxSales;           
+                    }
+                };
+
+                document.write("<h2>" + " The min sales of this hour is " + parisSales.minSales)
+                document.write("<h2>" + " The max sales of this hour is " + parisSales.maxSales)
+                document.write("<h2>" + " The average sales of this hour is " + parisSales.avgSales)
+                // Paris Object Literal Ends
+
+                // Lima Object Literal Begins
+                var limaSales = {
+                    minSales: 2, 
+                    maxSales: 16,
+                    avgSales: 4.6,
+                    purchases: function(){
+                        return this.minSales + this.maxSales;
+                    },
+                    totalSales : function(){
+                        return this.minSales + this.maxSales;           
+                    }
+                };
+
+                document.write("<h2>" + " The min sales of this hour is " + limaSales.minSales)
+                document.write("<h2>" + " The max sales of this hour is " + limaSales.maxSales)
+                document.write("<h2>" + " The average sales of this hour is " + limaSales.avgSales)
+                // Lima Object Literal Ends
+
